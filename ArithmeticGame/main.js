e83b20ca9d4602380552b461882f0fd3b26c0ab2
march-dave <ysangy@gmail.com>
2016-04-12 16:50:30 -0700

    Random number generate

--- ArithmeticGame/main.js
+++ ArithmeticGame/main.js
@@ -2,8 +2,16 @@
 
 document.addEventListener('DOMContentLoaded', init);
 
+var totalNumber = 0;
+
 function init(event) {
-	console.log('event:', event);
+	var firstNumber = document.getElementById('firstNumber').textContent = Math.floor((Math.random() * 9) + 1);
+	var secondNumber = document.getElementById('secondNumber').textContent = Math.floor((Math.random() * 9) + 1);
+
+	totalNumber = firstNumber + secondNumber;
+	var total = firstNumber + secondNumber;
+
+	document.getElementById('totalNumber').textContent = total;
 }
 
 var button = document.addEventListener('click', btnClicked);
