a51579534729b565172954838061878dba3be26c
march-dave <ysangy@gmail.com>
2016-04-12 23:47:22 -0700

    Four Operator add

--- ArithmeticGame/main.js
+++ ArithmeticGame/main.js
@@ -3,32 +3,118 @@
 document.addEventListener('DOMContentLoaded', init);
 
 var totalNumber = 0;
+var array = [];
+var str = "";
 
 function init(event) {
-	var firstNumber = document.getElementById('firstNumber').textContent = Math.floor((Math.random() * 9) + 1);
-	var secondNumber = document.getElementById('secondNumber').textContent = Math.floor((Math.random() * 9) + 1);
+	
+	var firstNumber = document.getElementById('firstNumber').textContent = Math.ceil(Math.random() * 9);
+	var secondNumber = document.getElementById('secondNumber').textContent = Math.ceil(Math.random() * 9);
 
-	totalNumber = firstNumber + secondNumber;
-	var total = firstNumber + secondNumber;
+	var operatorArray = ["+", "-", "*", "/"];
+	var operator;
 
-	document.getElementById('totalNumber').textContent = total;
+	operator = operatorArray[Math.floor((Math.random() * operatorArray.length))]
+	document.getElementById('sign').textContent = operator;
+
+	switch(operator)
+	{
+		case '+':
+				totalNumber = firstNumber + secondNumber;
+			break;
+		case '-':
+				totalNumber = firstNumber - secondNumber;
+			break;
+		case '*':
+				totalNumber = firstNumber * secondNumber;
+			break;
+		case '/':
+				totalNumber = Math.round(firstNumber / secondNumber);
+			break;
+		default:
+			break;
+	}
+
+	// expected value
+	document.getElementById('totalNumber').textContent = ' = ' + totalNumber;
 }
 
 var button = document.addEventListener('click', btnClicked);
 
 function btnClicked(event) {
-	console.log('Button Clicked:',  'Button Clicked');
 
-	console.log("event.innerText: " + event.target.innerText);
+	if(event.target.matches('button')) {
 
-	// get that number element
-	var numberElemement = document.getElementById('number');
-	console.log("numberElemement: " + numberElemement);
-	// figure out what number is inside
-	var numString = numberElemement.textContent;
-	var num = parseInt(numString);
-	num++;
+		var text = event.target.textContent;
 
-	// set the next to the new number
-	numberElemement.textContent = num;
+		var compareNum = 0;
+
+		switch(text) 
+		{
+			case '±':
+				if (str.length) {
+					str = parseInt(str) > 0 ? "-" + str : str.slice(1);
+
+					// console.log("str: " + str);
+				}
+				break;
+			case '=':
+
+				break;
+			case 'Clr':
+					str = "";
+				break;
+	
+			default:
+					str = str + text;
+					// array.push(text);
+				break;
+		}
+
+		if ( isNaN(text) && text === '=' ) {
+
+			if ( parseInt(totalNumber) === parseInt(str) ) {
+				document.getElementById('answer').textContent = " congratulation!!: " + parseInt(str);
+			} else {
+				document.getElementById('answer').textContent = "Answer is wrong, actuall answer is: " + totalNumber;
+			}
+
+			// array.forEach(function(c, i, a) {
+
+			// 	switch(text) 
+			// 	{
+			// 		case '+':
+			// 			compareNum = compareNum + c;
+			// 			break;
+			// 		case '-':
+
+			// 			break;
+			// 		case '*':
+
+			// 			break;
+			// 		case '/':
+
+			// 			break;
+					
+			// 		default:
+							
+			// 			break;
+			// 	}
+
+			// } );
+
+			// if ( parseInt(totalNumber) === parseInt(compareNum) ) {
+			// 	document.getElementById('answer').textContent = " congratulation!!: " + parseInt(compareNum);
+			// } else {
+			// 	document.getElementById('answer').textContent = "Answer is wrong, actuall answer is: " + totalNumber;
+			// }
+		}
+
+		if ( isNaN(text) && text === 'Clr' ) {
+			
+			array = [];
+			compareNum = 0;
+			document.getElementById('answer').textContent = '';
+		}
+	}
 }
