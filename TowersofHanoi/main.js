2e7d97c40ae4fef1327ebd7950cbd2c8f2da6d2f
march-dave <ysangy@gmail.com>
2016-04-14 08:54:36 -0700

    Tower and block move

--- TowersofHanoi/main.js
+++ TowersofHanoi/main.js
@@ -4,11 +4,27 @@ $(document).ready(init);
 
 
 function init() {
-
 	$('.shell').click(clickShell);
 }
 
+var $selectedBlock = "";
 function clickShell(event) {
+
+	if ($selectedBlock.length) { 
+		console.log("selected > 0");
+
+		var $selectedShell = $(this).find('.towerBase');
+		$($selectedShell).prepend($selectedBlock);
+
+		// $($topBlock).removeClass('selected');
+
+		// var $selectedShell = $('#middle');
+		// $("#middle").prepend($selectedBlock);
+		// $($selectedShell).prepend($selectedShell);
+
+		$selectedBlock = "";
+		return false;
+	}
 
 	var $topBlock = $(this).find('.block').first();
 
@@ -18,4 +34,22 @@ function clickShell(event) {
 
 	// console.log("$($topBlock) ", $($topBlock));
 	// $($topBlock)
+
+	// var $selectedBlock = $($topBlock).detach();
+	$selectedBlock = $($topBlock).detach();
+	
+	// console.log($selectedBlock);
+
+	// var $selectedShell = $(this).find('.shell');
+	// $($selectedShell).prepend($selectedBlock);
+	
+	// $($topBlock).removeClass('selected');
+
+	// var $selectedShell = $('#middle');
+	// $("#middle").prepend($selectedBlock);
+	// $($selectedShell).prepend($selectedShell);
+
+
+
+	// $( ".inner" ).prepend( "<p>Test</p>" );
 }
