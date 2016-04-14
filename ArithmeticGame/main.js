d99a9cf68a9f54bca63e01a08c98dbce1b68dbbd
march-dave <ysangy@gmail.com>
2016-04-13 18:33:03 -0700

    Init

--- ArithmeticGame/main.js
+++ ArithmeticGame/main.js
@@ -1,13 +1,22 @@
 'user strict';
 
 document.addEventListener('DOMContentLoaded', init);
+// document.addEventListener('keypress', btnClicked);
 
 var totalNumber = 0;
 var array = [];
 var calCulatorString = "";
 
 function init(event) {
-	
+	calculateValueSetup();
+	// keyBoardInput();
+}
+
+// function keyBoardInput() {
+// 	document.addEventListener('keypress', btnClicked);
+// }
+
+function calculateValueSetup() {
 	var firstNumber = document.getElementById('firstNumber').textContent = Math.ceil(Math.random() * 9);
 	var secondNumber = document.getElementById('secondNumber').textContent = Math.ceil(Math.random() * 9);
 
@@ -35,15 +44,24 @@ function init(event) {
 			break;
 	}
 
-	// expected value
 	document.getElementById('totalNumber').textContent = ' = ' + totalNumber;
+
+	// var d = new Date();
+	// document.getElementById('clockdiv').textContent = d.getSeconds();
+	// var timeOutID = setTimeout(function () { init() }, 10000 );
+	// clearTimeout(timeOutID);
 }
 
-var button = document.addEventListener('click', btnClicked);
+
+// var button = document.addEventListener('click', btnClicked);
+// var buttonK = document.addEventListener('keypress', btnClicked);
+document.addEventListener('click', btnClicked);
+document.addEventListener('keypress', btnClicked);
 
 function btnClicked(event) {
 
-	if(event.target.matches('button')) {
+	// if(event.target.matches('buttonM') || event.target.matches('buttonK')) {
+	if(event.target.matches('button') ) {
 
 		var text = event.target.textContent;
 
@@ -59,19 +77,20 @@ function btnClicked(event) {
 			case '=':
 
 				break;
-			case 'Clr':
+			case 'clr':
 					calCulatorString = "";
-					document.getElementById('skipAnswer').textContent = '';
+					document.getElementById('skipAnswerValue').textContent = '';
 
 				break;
 			case 'Skip':
 				init();
 
-				document.getElementById('skipAnswer').textContent = totalNumber;
+				document.getElementById('skipAnswer').style.display = "";
+				document.getElementById('skipAnswerValue').textContent = totalNumber;
+				break;
 	
 			default:
 					calCulatorString = calCulatorString + text;
-					// array.push(text);
 				break;
 		}
 
@@ -84,7 +103,7 @@ function btnClicked(event) {
 			}
 		}
 
-		if ( isNaN(text) && text === 'Clr' ) {
+		if ( isNaN(text) && text === 'clr' ) {
 			
 			array = [];
 			compareNum = 0;
