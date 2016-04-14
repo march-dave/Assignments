da09eabae1f7fe1bf7702261c5aa37be27c0b927
march-dave <ysangy@gmail.com>
2016-04-14 06:35:59 -0700

    Tower and block add jQuery started

--- TowersofHanoi/main.js
+++ TowersofHanoi/main.js
@@ -1,6 +1,21 @@
 'use strict';
 
+$(document).ready(init);
 
 
+function init() {
 
+	$('.shell').click(clickShell);
+}
 
+function clickShell(event) {
+
+	var $topBlock = $(this).find('.block').first();
+
+	// console.log("$topBlock ", $topBlock);
+
+	$($topBlock).addClass('selected');
+
+	// console.log("$($topBlock) ", $($topBlock));
+	// $($topBlock)
+}
